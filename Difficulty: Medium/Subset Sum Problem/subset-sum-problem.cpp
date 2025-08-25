4710a3793a5f82741c3da919c2b2a3cb2a8ba279
Aaditya Raj <adityaraj896919@gmail.com>
2025-08-25 13:30:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,8 +1,7 @@
 class Solution {
   public:
-  int t[200+1][10000+1];
+  int t[200+1][10000+5];
     bool isSubsetSum(vector<int>& arr, int sum) {
-        // code here
         int n = arr.size();
         for(int i=0;i<n+1;i++){
             for(int j=0;j<sum+1;j++){
@@ -10,11 +9,10 @@ class Solution {
                 if(j==0) t[i][j] = true;
             }
         }
-        
         for(int i=1;i<n+1;i++){
             for(int j=1;j<sum+1;j++){
                 if(arr[i-1] <= j){
-                    t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+                    t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j]; 
                 }else{
                     t[i][j] = t[i-1][j];
                 }
